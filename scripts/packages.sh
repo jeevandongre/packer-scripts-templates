8d1758e62818d09df8cbcc2df564d3594f142b87
jeevan <jeevan@jeevandongre.net>
2015-09-28 11:42:44 +0530

    installing mysql-server without password prompt

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -5,8 +5,10 @@ sudo apt-get update --fix-missing
 
 
 ##Installs basic packages for ubuntu 14.04
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -q -y install mysql-server
 
-sudo apt-get -y install build-essential zlib1g-dev libreadline-dev libssl-dev g++ bundler curl git htop  mysql-client libcurl3 libcurl3-gnutls libcurl4-openssl-dev liblzma-dev zlib1g-dev libmysqlclient-dev libsqlite3-dev nodejs nodejs-legacy npm 
+sudo apt-get -y install build-essential zlib1g-dev libreadline-dev libssl-dev g++ bundler curl git htop  mysql-client libcurl3 libcurl3-gnutls libcurl4-openssl-dev liblzma-dev zlib1g-dev libmysqlclient-dev libsqlite3-dev nodejs nodejs-legacy npm debconf-utils
 
 ##Installs ruby 2.0.0-p247 Please change the Url if you want to install another version of ruby
 sudo -u ubuntu  wget 'http://s3-ap-southeast-1.amazonaws.com/st-back-up/ruby-2.0.0-p247.tar.gz'
