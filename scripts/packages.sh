cae211d6c08eabe94844997f228cf946441e5c36
jeevan <jeevan@jeevandongre.net>
2015-09-29 18:37:02 +0530

    changed the script/packages to install mysql-server-5.6 mysql-client-5.6 along with debutilis and also installs chef-client

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,16 +1,12 @@
-#! /bin/bash -e
+#!/bin/bash -e
+
 sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get -y install linux-headers-$(uname -r)
 sudo apt-get update --fix-missing
 
 
-##Installs basic packages for ubuntu 14.04
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -q -y install mysql-server
+sudo apt-get -y install build-essential zlib1g-dev libreadline-dev libssl-dev g++ bundler curl git htop  mysql-client-5.6 libcurl3 libcurl3-gnutls libcurl4-openssl-dev liblzma-dev zlib1g-dev libmysqlclient-dev libsqlite3-dev nodejs nodejs-legacy npm debconf-utils
 
-sudo apt-get -y install build-essential zlib1g-dev libreadline-dev libssl-dev g++ bundler curl git htop  mysql-client libcurl3 libcurl3-gnutls libcurl4-openssl-dev liblzma-dev zlib1g-dev libmysqlclient-dev libsqlite3-dev nodejs nodejs-legacy npm debconf-utils
-
-##Installs ruby 2.0.0-p247 Please change the Url if you want to install another version of ruby
 sudo -u ubuntu  wget 'http://s3-ap-southeast-1.amazonaws.com/st-back-up/ruby-2.0.0-p247.tar.gz'
 echo "Downloading ruby from s3..... \n  Please wait ......"
 sudo -u ubuntu /bin/tar -xvf ruby-2.0.0-p247.tar.gz
@@ -22,3 +18,13 @@ sudo -u ubuntu make test
 sudo make install
 sudo /usr/bin/npm install pm2 -g
 
+dbpass="admin"
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+apt-get -y install mysql-server-5.6
+
+cd /home/ubuntu/
+
+wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.4.3-1_amd64.deb
+sudo /usr/bin/dpkg --install chef_12.4.3-1_amd64.deb
